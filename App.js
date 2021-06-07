efe042ec06239813dc975c61b08bde2dd97a31d5
Zaqueu Souza <39178924+zaccsouza@users.noreply.github.com>
2021-06-07 17:58:49 -0300

    Hamburger Menu Button Implementation
    Implementação do botão de menu estilo hamburguer.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -16,31 +16,56 @@ const Stack = createStackNavigator();
 function myHome({navigation}){
   return(
       <Stack.Navigator screenOptions={{headerStyle:{backgroundColor: '#111111',}, headerTintColor:'#eee'}}>
-        <Stack.Screen name = "Home" component={Home} options={{ headerRight: () => (
-          <Button onPress={() => navigation.openDrawer()} title="Info" color="#fff"
-          />
-          ),
-        }} />
+        <Stack.Screen 
+          name = "Home" 
+          component={Home} 
+          options={{ 
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.openDrawer()} style={styles.menubutton}>
+                <Image source={require('./assets/hamburguer-icon.png')} style={styles.iconstyle}/>
+              </TouchableOpacity>  
+            ),
+          }} 
+        />
       </Stack.Navigator>
   );
 }
 
-function myAgentes(){
+function myAgentes({navigation}){
   return(
     <Stack.Navigator screenOptions={{headerStyle:{backgroundColor: '#111111',}, headerTintColor:'#eee'}}>
-        <Stack.Screen name = "Agente" component={Agentes} />
+        <Stack.Screen 
+          name = "Agente" 
+          component={Agentes}
+          options={{
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.openDrawer()} style={styles.menubutton}>
+                <Image source={require('./assets/hamburguer-icon.png')} style={styles.iconstyle}/>
+              </TouchableOpacity>  
+            ),
+          }}
+        />
     </Stack.Navigator>
   );
 }
 
-function myDetentos(){
+function myDetentos({navigation}){
   return(
     <Stack.Navigator screenOptions={{headerStyle:{backgroundColor: '#111111',}, headerTintColor:'#eee'}}>
-        <Stack.Screen name = "Detentos" component={Detentos} />
+        <Stack.Screen 
+          name = "Detentos" 
+          component={Detentos}
+          options={{
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.openDrawer()} style={styles.menubutton}>
+                <Image source={require('./assets/hamburguer-icon.png')} style={styles.iconstyle}/>
+              </TouchableOpacity>  
+            ),
+          }}
+        />
     </Stack.Navigator>
   );
 }
-
 
 // Menu de Navegação Drawer
 
@@ -62,6 +87,20 @@ export default function App() {
     </NavigationContainer>
   );  
 }
+
+const styles = StyleSheet.create({
+  menubutton: {
+    backgroundColor: '#111',
+    height: 40,
+    width: 40,
+    marginRight: 5,
+  },
+  iconstyle: {
+    width: 25,
+    height: 25,
+    marginTop:10
+  },
+});
 
 // O initialRouteName="Login", tem como finalidade deixar como default a tela de Login ,
 // Por exemplo caso o usuario queira forçar que uma tela seja chamada primeiro usa esse status.
